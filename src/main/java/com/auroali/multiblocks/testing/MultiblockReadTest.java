b48d27c84869d4797d1bf270c49b42252b26f520
Auroali <dawn.auroali@gmail.com>
2024-12-17 14:28:37 -0400

    remove useless class

--- src/main/java/com/auroali/multiblocks/testing/MultiblockReadTest.java
+++
@@ -1,8 +0,0 @@
-package com.auroali.multiblocks.testing;
-
-import com.auroali.multiblocks.MultiblockHolder;
-import net.minecraft.util.Identifier;
-
-public class MultiblockReadTest {
-    public MultiblockHolder TEST = MultiblockHolder.getOrCreate(new Identifier("multiblocks", "test_structure"));
-}
